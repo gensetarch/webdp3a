bf59c2750c0e69660881b2e6bca24d94ade9f8b7
ariniarini2207-arch <ariniarini2207@gmail.com>
2026-07-09 14:26:36 +0000

    Deploying to gh-pages from @ ariniarini2207-arch/webdp3a@8c7b224ecf2a9ccfdfd3e40ada7bc9b98e8e5114 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3443426058" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2608881837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
