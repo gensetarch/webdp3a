47104ea8745a9a253e6ce8f48bb21f6776b0fb8d
gensetarch <ariniarini2207@gmail.com>
2026-07-22 03:20:06 +0000

    Deploying to gh-pages from @ gensetarch/webdp3a@5ecef479eaefcd510e2887d3dc41626ae9d7384a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "626820390" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3443458703" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
